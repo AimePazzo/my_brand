19beef71ec20bb108a28ebfdeae4360419c0dfd5
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-03-13 15:02:32 +0200

    Email Validation complete

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const setSuccess = (element) => {
 };
 
 const isValidEmail = (email) => {
-  const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\.com$/;
+  const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return re.test(String(email).toLowerCase());
 };
 
